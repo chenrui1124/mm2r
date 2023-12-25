fdf0aefa0eb6b5c03d3f38d3f6d6946d0249372a
Rui <75087533+chenrui1124@users.noreply.github.com>
2023-12-25 23:25:07 +0800

    Composable: Update useIterate

--- lib/composable/use-iterate.ts
+++ lib/composable/use-iterate.ts
@@ -1,8 +1,8 @@
 import { computed, ref } from 'vue'
 
-export function useIterate<ItemType>(each: ItemType[] | readonly ItemType[], init: number = 0) {
+export function useIterate<T>(each: T[] | readonly T[], init: number = 0) {
     const index = ref<number>(init < each.length && init >= 0 ? init : 0),
-        item = computed<ItemType>(() => each[index.value])
+        item = computed<T>(() => each[index.value])
 
     function $next() {
         index.value = (index.value + 1) % each.length
@@ -12,9 +12,11 @@ export function useIterate<ItemType>(each: ItemType[] | readonly ItemType[], ini
         requestAnimationFrame ? requestAnimationFrame($next) : $next()
     }
 
-    function nextWith(run: (item?: ItemType) => void) {
-        next()
-        run(item.value)
+    function nextWith(run: (item?: T, index?: number) => void) {
+        return () => {
+            next()
+            run(item.value, index.value)
+        }
     }
 
     return { item, index, next, nextWith }
