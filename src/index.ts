7a523f5c1af847200b1859c3b2700edea92c8e87
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-02-22 14:58:24 +0800

    + Either

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never }
+
+export type Either<T, U> = T | U extends object ? (T & Without<U, T>) | (U & Without<T, U>) : T | U
+
 export type Equal<A, B> = A extends B ? (B extends A ? true : never) : never
 
 export type MaybeReadonly<T> = T | Readonly<T>
